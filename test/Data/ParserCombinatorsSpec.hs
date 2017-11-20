5f0af8eebb1e0b6d7f84a5a3793147f943863ad6
ebenpack <ebenpackwood@gmail.com>
2017-11-20 18:42:02 -0500

    Add tests

--- test/Data/ParserCombinatorsSpec.hs
+++ test/Data/ParserCombinatorsSpec.hs
@@ -16,12 +16,29 @@ spec :: Spec
 spec = do
   describe "ParserCombinators" $ do
     describe "failure" $ do
-      it "returns a ParseError" $
+      it "should return a ParseError" $
         property $ \x ->
           (parse failure x) ==
           ((Left (ParseError "Error")) :: ParseResult String)
-    -- describe "item" $ do it "should do stuff" $ do 1 `shouldBe` 1
-    -- describe "(<|>)" $ do it "should do stuff" $ do 1 `shouldBe` 1
+    describe "item" $ do
+      it "should take a single char" $
+        property $ \x ->
+          let result = parse item x
+          in case x of
+               [] -> result == ((Left (ParseError "Error")) :: ParseResult Char)
+               (y:ys) -> result == (Right [(y, ys)] :: ParseResult Char)
+    describe "(<|>)" $ do
+      it "should accept the first argument that succeeds, and otherwise" $
+        property $ \x ->
+          let result1 = parse (item <|> failure) x
+              result2 = parse (failure <|> item) x
+          in case x of
+               [] ->
+                 result1 == result2 &&
+                 result1 == ((Left (ParseError "Error")) :: ParseResult Char)
+               (y:ys) ->
+                 result1 == result2 &&
+                 result1 == (Right [(y, ys)] :: ParseResult Char)
     -- describe "sat" $ do it "should do stuff" $ do 1 `shouldBe` 1
     -- describe "rej" $ do it "should do stuff" $ do 1 `shouldBe` 1
     -- describe "digit" $ do it "should do stuff" $ do 1 `shouldBe` 1
