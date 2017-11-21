9c10c6053e4c7f4cbf6b80f0d9ec25e78ddf7e5a
ebenpack <ebenpackwood@gmail.com>
2017-11-20 19:00:05 -0500

    Reorg

--- test/Data/ParserCombinatorsSpec.hs
+++ test/Data/ParserCombinatorsSpec.hs
@@ -39,6 +39,12 @@ spec = do
                (y:ys) ->
                  result1 == result2 &&
                  result1 == (Right [(y, ys)] :: ParseResult Char)
+    -- describe "many'" $ do it "should do stuff" $ do 1 `shouldBe` 1
+    -- describe "many1" $ do it "should do stuff" $ do 1 `shouldBe` 1
+    -- describe "skipMany" $ do it "should do stuff" $ do 1 `shouldBe` 1
+    -- describe "skipMany1" $ do it "should do stuff" $ do 1 `shouldBe` 1
+    -- describe "oneOf" $ do it "should do stuff" $ do 1 `shouldBe` 1
+    -- describe "noneOf" $ do it "should do stuff" $ do 1 `shouldBe` 1
     -- describe "sat" $ do it "should do stuff" $ do 1 `shouldBe` 1
     -- describe "rej" $ do it "should do stuff" $ do 1 `shouldBe` 1
     -- describe "digit" $ do it "should do stuff" $ do 1 `shouldBe` 1
@@ -51,9 +57,3 @@ spec = do
     -- describe "space" $ do it "should do stuff" $ do 1 `shouldBe` 1
     -- describe "char" $ do it "should do stuff" $ do 1 `shouldBe` 1
     -- describe "string" $ do it "should do stuff" $ do 1 `shouldBe` 1
-    -- describe "many'" $ do it "should do stuff" $ do 1 `shouldBe` 1
-    -- describe "many1" $ do it "should do stuff" $ do 1 `shouldBe` 1
-    -- describe "skipMany" $ do it "should do stuff" $ do 1 `shouldBe` 1
-    -- describe "skipMany1" $ do it "should do stuff" $ do 1 `shouldBe` 1
-    -- describe "oneOf" $ do it "should do stuff" $ do 1 `shouldBe` 1
-    -- describe "noneOf" $ do it "should do stuff" $ do 1 `shouldBe` 1
